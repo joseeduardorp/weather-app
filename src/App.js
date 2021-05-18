830527f59fbe39a527d63912a2247bbf2ae01bd9
José Eduardo <joseeduardorperes@gmail.com>
2021-05-18 11:27:15 -0300

    retriving and using API data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
+import { useState, useEffect } from 'react';
+
+const fields = "only_results, condition, description, city_name, temp, forecast, max, min, weekday";
+
 export default function App() {
+  const [infos, setInfos] = useState({});
+
+  async function fetchInfos (longitude, latitude) {
+    const lat = latitude.toFixed(2);
+    const lon = longitude.toFixed(2);
+
+    const url = `
+    https://api.hgbrasil.com/weather?locale=pt&lat=${lat}&lon=${lon}&array_limit=7&fields=${fields}&key=fc6021ed
+    `;
+
+    const response = await fetch(url)
+    const data = await response.json();
+
+    setInfos(data);
+  }
+
+  useEffect(() => {
+    console.clear();
+
+    navigator
+      .geolocation
+      .getCurrentPosition(
+        (position) => fetchInfos(position.coords.longitude, position.coords.latitude), 
+        () => console.log("error")
+      );
+  }, []);
+
+  console.log(infos);
+
   return (
     <>
-      <h1>Weather App</h1>
+      <header>
+        <h1>Weather App</h1>
+      </header>
+
+      <div className="appContainer">
+        <div className="currentDay">
+          <h2>{infos.description}</h2>
+          <p>{infos.city_name}</p>
+          <span>{infos.temp}°</span>
+
+            <div>
+              <p><strong>Max:</strong>{infos.forecast[0].max}°</p>
+              <div>|</div>
+              <p><strong>Min:</strong>{infos.forecast[0].min}°</p>
+            </div>
+          </div>
+
+          <div className="anotherDays">
+          {
+            infos.forecast.map(({weekday, max, min}, index) => {
+              return (
+                <div key={index}>
+                  <h3>{weekday}</h3>
+                  <div>
+                    <p><strong>Max:</strong> {max}°</p>
+                    <p><strong>Min:</strong> {min}°</p>
+                  </div>
+                </div>
+              );
+            })
+          }
+          </div>
+      </div>
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,42 @@
+module.exports = {
+  condition: "nublado",
+  city: "são josé dos campos",
+  temp: 19,
+  forecast: [
+    {
+      day: "ter",
+      max: 22,
+      min: 12,
+    },
+    {
+      day: "qua",
+      max: 25,
+      min: 14,
+    },
+    {
+      day: "qui",
+      max: 20,
+      min: 9,
+    },
+    {
+      day: "sex",
+      max: 23,
+      min: 13,
+    },
+    {
+      day: "sab",
+      max: 19,
+      min: 11,
+    },
+    {
+      day: "dom",
+      max: 18,
+      min: 13,
+    },
+    {
+      day: "seg",
+      max: 22,
+      min: 15,
+    },
+  ]
+}
